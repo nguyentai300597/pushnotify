a039791db56e97fd7b86114ecae5536a94d8f8da
nguyentai300597 <tai.nguyen@unibiz.io>
2022-02-23 11:24:41 +0700

    testpush

---
+++ android/app/src/main/kotlin/com/example/pushnotifi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pushnotifi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/note/notepage.dart
@@ -0,0 +1,26 @@
+// https://fcm.googleapis.com/fcm/send
+
+// Content-Type= application/json
+// Authorization=AIzaSyAEb3NhsfGw1ih5mn8cmrGUAXMp86waQ //FCM SERVER KEY
+
+
+// { 
+// "to":"eva_m35mTe-w_OzRGgryVi:APA91bHleym28AW71xSq7TVgkTyQXt3rpxoYscB_TFROVrf4RzJnxTYjs_3RtOpZE5ral8rFGHux4VkyUoq_D09HU2DySRlOfgtqjdO1J0xfl0WrX1Msa0GWtcoS2rsYsc0ZDD7Km3g4", 
+//  "notification" : {
+//      "title":"name",
+//  "body" : "New announcement assigned",
+//  "OrganizationId":"2",
+//  "content_available" : true,
+//  "priority" : "high",
+//  "subtitle":"Elementary School",
+//  "Title":"hello",
+//  "description":"213213"
+//  },
+//  "data" : {
+//  "priority" : "high",
+//  "sound":"app_sound.wav",
+//  "content_available" : true,
+//  "bodyText" : "New Announcement assigned",
+//  "organization" :"Elementary school"
+// }
+// }
